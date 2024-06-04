17d141ba8f8a47d8c4022a493885a53110f651fd
Brunopeace <137825826+Brunopeace@users.noreply.github.com>
2024-06-04 18:36:46 -0300

    Atualizar o script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
 function verificarAcesso() {
-            const uuidEsperado = 'fc30c781-e382-406b-b65a-4e850382e014';
+            const uuidEsperado = '';
             let uuidArmazenado = localStorage.getItem('uuid');
 
             if (!uuidArmazenado) {
