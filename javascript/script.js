a7c15804dada280c66ee37c02d91e2298dc842d5
Brunopeace <137825826+Brunopeace@users.noreply.github.com>
2024-07-07 21:44:03 -0300

    Atualizar o script.js

--- javascript/script.js
+++ javascript/script.js
@@ -57,7 +57,7 @@ function verificarLogoComemorativa() {
         logo.src = 'img/logo-independencia.png';
         
         
-    } else if (mes === 10 && dia === 31) { // Halloween
+    } else if (mes === 7 && dia === 7) { // Halloween
         logo.src = 'img/logo-halloween.png';
         
         
