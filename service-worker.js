a56cd75588ad9997239599d1c5e822a439a4374a
Brunopeace <137825826+Brunopeace@users.noreply.github.com>
2024-08-08 20:34:02 -0300

    Atualizar o service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,48 +2,24 @@ const CACHE_NAME = 'clientes-cache-v1';
 const urlsToCache = [
   '/',
   '/css/estilo.css',
-  '/javascript/script.js',
-  '/img/logo-padrao.png',
-  '/service-worker.js',
-  '/index.html'
+  '/manifest.json',
+  // adicione outros recursos que deseja armazenar em cache
 ];
 
-// Evento de instalação do Service Worker
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        console.log('Cache aberto');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-// Evento de ativação do Service Worker
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Evento de fetch para interceptar requisições e servir arquivos do cache
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        if (response) {
-          return response; // Arquivo encontrado no cache
-        }
-        return fetch(event.request); // Arquivo não encontrado no cache, fazer requisição à rede
+        return response || fetch(event.request);
       })
   );
 });
