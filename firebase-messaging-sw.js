37f78294858435a896c4ab32248146194e41d8e8
Brunopeace <137825826+Brunopeace@users.noreply.github.com>
2025-11-14 16:52:51 -0300

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,21 @@
+importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+    apiKey: "AIzaSyCw38sezt3TWmFIuUXFD3mPAj2ZPF6ZD4o",
+    authDomain: "gerenciador-eeb96.firebaseapp.com",
+    databaseURL: "https://gerenciador-eeb96-default-rtdb.firebaseio.com",
+    projectId: "gerenciador-eeb96",
+    storageBucket: "gerenciador-eeb96.appspot.com",
+    messagingSenderId: "560345055072",
+    appId: "1:560345055072:web:9048475b7ac90a7f43e805"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(payload => {
+    self.registration.showNotification(payload.notification.title, {
+        body: payload.notification.body,
+        icon: "/img/icon-192.png"
+    });
+});
