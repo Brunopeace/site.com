e665e27c7f384bdd5523ff06722875c5b9f60edb
Brunopeace <137825826+Brunopeace@users.noreply.github.com>
2025-11-17 10:16:21 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,65 +1,58 @@
-self.addEventListener('install', () => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', () => {
-  clients.claim();
-});
-
-// Nome do cache
-const CACHE_NAME = 'gerenciador-de-clientes-v2';
-
-// Arquivos a serem cacheados
-const urlsToCache = [
-    '/',
-    '/index.html',
-    '/css/styles.css',
-    '/img/icon192.png',
-    '/img/icon512.png',
-    '/img/iconpadrao1024.png.png',
-    '/js/script.js'
-];
-
-// Instala o Service Worker e faz o cache dos arquivos estáticos
+// Forçar ativação imediata do SW novo
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                console.log('Service Worker: fazendo cache dos arquivos');
-                return cache.addAll(urlsToCache);
-            })
-    );
-});
+  self.skipWaiting(); // força ativação imediata
 
-// Ativa o Service Worker e remove caches antigos
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cache) => {
-                    if (cache !== CACHE_NAME) {
-                        console.log('Service Worker: removendo cache antigo', cache);
-                        return caches.delete(cache);
-                    }
-                })
-            );
+  const CACHE_NAME = 'gerenciador-de-clientes-v2';
+
+  const urlsToCache = [
+      '/',
+      '/index.html',
+      '/css/styles.css',
+      '/img/icon192.png',
+      '/img/icon512.png',
+      '/img/iconpadrao1024.png',
+      '/js/script.js'
+  ];
+
+  event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(cache => {
+            console.log('Service Worker: fazendo cache dos arquivos');
+            return cache.addAll(urlsToCache);
         })
-    );
+  );
 });
 
-// Intercepta requisições e responde com os arquivos do cache
+// Ativar imediatamente e assumir controle das abas abertas
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+      caches.keys().then((cacheNames) => {
+          return Promise.all(
+              cacheNames.map((cache) => {
+                  if (cache !== 'gerenciador-de-clientes-v2') {
+                      console.log('Service Worker: removendo cache antigo', cache);
+                      return caches.delete(cache);
+                  }
+              })
+          );
+      })
+  );
+
+  clients.claim(); // força assumir o controle
+});
+
+// Responder com o cache
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                if (response) {
-                    return response; // Se encontrar no cache, retorna o cache
-                }
-                console.log('Service Worker: arquivo não encontrado no cache, fazendo requisição:', event.request.url);
-                return fetch(event.request); // Se não encontrar no cache, faz a requisição normalmente
-            }).catch(() => {
-                // Aqui você pode adicionar uma página offline personalizada
-                return caches.match('/index.html');
-            })
-    );
+  event.respondWith(
+      caches.match(event.request)
+          .then((response) => {
+              if (response) {
+                  return response;
+              }
+              console.log('Service Worker: arquivo não encontrado no cache:', event.request.url);
+              return fetch(event.request);
+          }).catch(() => {
+              return caches.match('/index.html');
+          })
+  );
 });
